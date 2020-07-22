d75c80c8ce8163e0940eef4465617a9d9a556246
Jeffrey Carr <jeffrey.carr98@gmail.com>
2020-07-21 22:16:01 -0400

    Updated home page, set up router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing modules
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Router } from '@angular/router';
+
+// Components
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { MenuBarComponent } from './global_components/menu-bar/menu-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+// Custom modules
+import { NbThemeModule, NbCardModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'page-not-found', component: PageNotFoundComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: '**', redirectTo: '/page-not-found', pathMatch: 'full'}
+    ]),
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NbThemeModule.forRoot(),
+    NbCardModule,
+    NbLayoutModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

c3d0a0320fa0de67f98e034732fa6f1ea21b9604
jcarr98 <jjcarr@umas.edu>
2020-09-22 20:41:25 -0400

    Set up projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+    allProjects: String[]
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.allProjects = [
+            "Personal Website",
+            "Personal Website (old)",
+            "Automatic Window Shades",
+            "Planner",
+            "Mod Remover"
+        ]
+    }
 
 }
